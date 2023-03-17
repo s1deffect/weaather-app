696600832e842107bdff4a71118f51d897e7a44d
unknown <holyhera0@gmail.com>
2023-03-17 21:58:59 +0200

    edit

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
 console.log("hello world i am ahmed workingn from home ")
 
+
+const button = document.querySelector('#submit');
+button.addEventListener("click", e => {
+    e.preventDefault();
+    load();
+})
+
+
+//fetching the data from the api server 
 const getData = async (city) => {
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=57b4c07506dd2c5d50ee2bf7a7ba03c8')
-    console.log(response)
     const result = await response.json();
-    console.log(result)
+    return result
 }
-getData("egypt");
+
+// the main function 
+function load () {
+    const city = document.querySelector("#search").value
+    getData(city)
+    .then((data) => {
+        console.log(data)
+        updateUi(data)   
+})
+}
+
+// update the userinterface
+const updateUi = (data) => {
+    let d = new Date();
+    let newDate = d.getMonth()+ 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
+    const statue = document.querySelector(".statue");
+    const city = document.querySelector(".city");
+    const date = document.querySelector(".date");
+    const temp = document.querySelector(".temp");
+    const minMax = document.querySelector(".min-max");
+    const like = document.querySelector(".like");
+    const humdity = document.querySelector(".humdity");
+    const pressure = document.querySelector(".pressure");
+    statue.textContent = data["weather"] [0] ["main"];
+    city.textContent = data["name"];
+    date.textContent = `Today ${newDate}` ;
+    temp.textContent = ` Temperautre: ${Math.round(data["main"]["temp"])} C`;
+    minMax.textContent = ` Max: ${Math.round(data["main"]["temp_max"])} `+`min: ${Math.round(data["main"]["temp_max"])}`;
+    like.textContent = ` Feels-like: ${data ["main"] ["feels_like"]} c`;
+    humdity.textContent = ` humidity: ${data ["main"] ["humidity"]}`;
+    pressure.textContent = `pressure: ${data["main"]["pressure"]}`
 
 
 
-// const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=57b4c07506dd2c5d50ee2bf7a7ba03c8')
+
+
+    
+
+    
+}
+
 
